b92ed19161e556fe922f501f5dee48c8c1370030
Eureka <2395505896@qq.com>
2023-02-11 17:45:15 +0800

    master

--- TheImplementationOfGraph/DijkstraAlgorithm.cpp
+++ TheImplementationOfGraph/DijkstraAlgorithm.cpp
@@ -2,10 +2,56 @@
 #include<vector>
 #include"GraphClass.h"
 using namespace std;
+// 算法过程
+// 设置出发顶点为v，顶点集合为V{v1,v2,vi...}，v到V中个顶点的距离构成距离集合Dis
+// Dis{d1,d2,di...}，Dis集合记录着v到图中各顶点的距离（v到vi的距离记为di）
+// 1.从Dis中选择值最小的di并移出Dis集合，同时移出V集合中对应的顶点vi，此时v到vi即为最短路径
+// 2.更新Dis集合，比较v到V集合中顶点的距离值，与v通过vi到V集合中顶点的距离值，保留值较小的一个
+// 3.重复执行两步骤，直到最短路径顶点为目标顶点即可结束
+void creatGraph(Graph&graph,int numOfVertex,char* data,int**weight){
+    for(int i=0;i<numOfVertex;++i){
+        graph.insertVertex(data[i]);
+        for(int j=i+1;j<numOfVertex;++j){
+            graph.insertEdge(i,j,weight[i][j]);
+            }
+        }
+}
+
+void Dijkstra(int index,Graph& graph){
+    graph.setStart(index);
+    graph.update(index);
+    for(int j=1;j<graph.getNumOfVertex();j++){
+        index=graph.updateArr();
+        graph.update(index);
+    }
+}
+
 int main(){
-    
-
-
+    Graph graph(7);
+    char data[7]={'A','B','C','D','E','F','G'};
+    char*ptr1=data;
+    //大数表示两点不连通
+    int weight[7][7]=
+    {{INF,5,7,INF,INF,INF,2},
+    {5,INF,INF,9,INF,INF,3},
+    {7,INF,INF,INF,8,INF,INF},
+    {INF,9,INF,INF,INF,4,INF},
+    {INF,INF,8,INF,INF,5,4},
+    {INF,INF,INF,4,5,INF,6},
+    {2,3,INF,INF,4,6,INF}};
+    int**ptr2=new int*[7];
+    for(int i=0;i<7;++i){
+        *(ptr2+i)=new int[7];
+    }
+    for(int i=0;i<7;++i){
+        for(int j=0;j<7;++j){
+            ptr2[i][j]=weight[i][j];
+        }
+    }
+    creatGraph(graph,7,ptr1,ptr2);
+    graph.printGraph();
+    Dijkstra(6,graph);
+    graph.printList();
     system("pause");
     return 0;
 }
--- TheImplementationOfGraph/GraphClass.h
+++ TheImplementationOfGraph/GraphClass.h
@@ -1,21 +1,30 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+const int INF=10000;
 class Graph{
 private:
     vector<char>vertexList;//存储顶点集合 
     vector<bool>isVisited;//存储结点是否被访问的标志
     vector<vector<int>>edges;
-    int numOfEdges;
+    vector<int>pre_visited;//每个下标对应的值为前一个结点下标
+    vector<int>dis;//记录出发顶点到其他所有顶点的最短距离
+    int numOfEdges=0;
+    int startIndex=0;//出发顶点下标
 
 public:
     Graph(int numOfVertex){
         //初始化矩阵与vertexList
         this->edges=vector<vector<int>>(numOfVertex,vector<int>(numOfVertex,0));
-        // this->vertexList=vector<char>(numOfVertex);
-        isVisited.resize(numOfVertex);
-        fill(isVisited.begin(),isVisited.end(),false);
-        this->numOfEdges=0;
+        //初始化isVisited
+        this->isVisited.resize(numOfVertex);
+        fill(this->isVisited.begin(),this->isVisited.end(),false);
+        //初始化pre_visited
+        this->pre_visited.resize(numOfVertex);
+        fill(this->pre_visited.begin(),this->pre_visited.end(),0);
+        //初始化距离数组
+        this->dis.resize(numOfVertex);
+        fill(this->dis.begin(),this->dis.end(),INF);
     }
     //根据前一个邻接结点的下标获取下一个邻接结点
     int getNextAdjacentVertexIndex(int v1,int v2){
@@ -36,9 +45,11 @@ public:
         }
         return -1;
     }
+    //对顶点进行访问操作
     void visit(int index){
         this->isVisited[index]=true;
     }
+    //判断顶点是否被访问过
     bool beVisited(int index){
         return this->isVisited[index];
     }
@@ -54,21 +65,83 @@ public:
         }else{
             this->edges[v1][v2]=weight;
         }
-        if(weight!=10000){
+        if(weight!=INF){
             this->numOfEdges++;
         }
     }
+    //更新出发顶点到index顶点的距离
+    void updateDis(int index,int len){
+        this->dis[index]=len;
+    }
+    //更新index这个顶点的前驱顶点为pre
+    void updatePre(int index,int pre){
+        this->pre_visited[index]=pre;
+    }
+    //返回出发顶点到index顶点的距离
+    int getDis(int index){
+        return this->dis[index];
+    }
+    //设置出发顶点下标
+    void setStart(int index){
+        this->startIndex=index;
+        this->dis[this->startIndex]=0;
+    }
+    //更新index下标顶点到周围顶点的距离和周围顶点的前驱顶点
+    void update(int index){
+        int len=0;
+        this->visit(index);
+        for(int j=0;j<this->vertexList.size();j++){
+            //len为出发顶点到index顶点的距离+从index顶点到j顶点的距离
+            len=this->getDis(index)+edges[index][j];
+            if(!beVisited(j)&&len<getDis(j)){
+                updateDis(j,len);
+                updatePre(j,index);
+            }
+        }
+    }
+    //继续选择并返回新的访问顶点
+    int updateArr(){
+        int min=INF,index=0;
+        for(int i=0;i<this->isVisited.size();i++){
+            if(this->isVisited[i]==0&&dis[i]<min){
+                min=this->dis[i];
+                index=i;
+            }
+        }
+        return index;
+    }
     void printGraph(){
         cout<<"图的邻接矩阵如下"<<endl;
-        for(auto i:edges){
+        for(auto i:this->edges){
             for(auto j:i){
                 cout<<j<<" ";
             }
             cout<<endl;
         }
+        cout<<"图的顶点数组如下"<<":";
         for(auto i:this->vertexList){
             cout<<i<<" ";
         }
+        cout<<endl;
+    }
+    void printList(){
+        cout<<"图的起始顶点为:"<<this->vertexList[this->startIndex];
+        cout<<endl;
+        cout<<"is_visited数组:";
+        for(auto i:this->isVisited){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        cout<<"pre_visited数组:";
+        for(auto i:this->pre_visited){
+        cout<<i<<" ";
+        }
+        cout<<endl;
+        cout<<"dis数组:";
+        for(int i=0;i<this->dis.size();i++){
+            cout<<this->vertexList[i]<<"("<<this->dis[i]<<")";
+        }
+        cout<<endl;
     }
     int getNumOfVertex(){
         return this->vertexList.size();
